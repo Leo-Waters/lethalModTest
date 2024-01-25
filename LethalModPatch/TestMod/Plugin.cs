4650b2ccb1d20d80375dbc7269b839aa3a9225c9
Lazy_Boy <li0nleo117@gmail.com>
2024-01-25 16:31:12 +0000

    daz

---
+++ LethalModPatch/TestMod/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalLib.Modules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace TestMod
+{
+    [BepInPlugin(ModGUID, ModName,ModVersion)]
+    public class TestModPatch : BaseUnityPlugin
+    {
+        const string ModGUID="lh.testmod";
+        const string ModName="testmod";
+        const string ModVersion="1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(ModGUID);
+
+        private static TestModPatch Instance;
+
+        internal ManualLogSource logger;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+
+            string assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "dazblock");
+            AssetBundle bundle = AssetBundle.LoadFromFile(assetDir);
+
+            Item dazBlock = bundle.LoadAsset<Item>("Assets/DazBlockMod/dazblock.asset");
+
+            NetworkPrefabs.RegisterNetworkPrefab(dazBlock.spawnPrefab);
+            Utilities.FixMixerGroups(dazBlock.spawnPrefab);
+            Items.RegisterScrap(dazBlock, 1000, Levels.LevelTypes.All);
+
+            TerminalNode node = ScriptableObject.CreateInstance<TerminalNode>();
+            node.clearPreviousText = true;
+            node.displayText = "Daz block\n\n";
+            Items.RegisterShopItem(dazBlock, null, null, node, 0);
+
+
+
+            logger.LogInfo(ModName+" has initalized");
+        }
+
+
+    }
+}
